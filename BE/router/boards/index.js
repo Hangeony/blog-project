0a6ecab393fd8edcb8015fdc30e9e5625d303b8d
Hangeony <geonhui95@naver.com>
2025-05-10 01:33:06 +0900

    중복 된 매소드 삭제

--- BE/router/boards/index.js
+++ BE/router/boards/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import * as boardsController from "./boards.controller.js";
 const router = express.Router();
-router.use(express.json()); //json 모듈활성화
 
 router.post("/posts", boardsController.create);
 
